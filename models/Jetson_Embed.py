1215d429d42f4ab95049becbfaeb78599905ce65
emrullahardc <emrullahardc@gmail.com>
2024-10-21 04:39:22 +0300

    bug-fix

--- models/Jetson_Embed.py
+++ models/Jetson_Embed.py
@@ -5,9 +5,13 @@ class JetsonEmbed:
     def trigger_relays(self, ip, port, relay_number=None, duration=None):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             client.connect((ip, port))
-            client.sendall(relay_number.encode())
-            print(f"Relay triggered relay number {relay_number}")
+            if relay_number is not None:
+                # Convert relay_number to a string before encoding
+                client.sendall(str(relay_number).encode())
+                print(f"Relay triggered: relay number {relay_number}")
 
-            data = client.recv(1024)
-            print(f"Response: {data.decode()}")
+                data = client.recv(1024)
+                print(f"Response: {data.decode()}")
+            else:
+                print("No relay number provided.")
             return True
